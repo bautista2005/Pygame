acd22e2ce1df4bdf0cba125bc91ab3c920b6edbd
bautista2005 <bachuv07@gmail.com>
2022-07-19 14:07:49 -0300

    actualizacion

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import sys
 import pygame
 
+def display_score():
+    current_time = pygame.time.get_ticks()
+    print(current_time)
+
 pygame.init()#inizialice pygame, all the elements of pygame
 
 #dispay surface, is the screen we are going to see
@@ -8,16 +12,16 @@ pygame.init()#inizialice pygame, all the elements of pygame
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption("Runner")#Changing the window name
 
-clock = pygame.time.Clock()#creating an objet clockx
+clock = pygame.time.Clock()#creating an objet clock
+
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)#(font type, font size)  
+#score_surf = test_font.render('My game', False, 'black')
+#score_rect = score_surf.get_rect(center= (400, 50))
 
 game_active = True
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert_alpha()
 ground_surface = pygame.image.load('graphics/ground.png').convert_alpha()
-
-score_surf = test_font.render('My game', False, 'black')
-score_rect = score_surf.get_rect(center= (400, 50))
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(midbottom= (800, 300))
@@ -32,11 +36,11 @@ while True:
     #for loop, we will be looking for all player inputs until the input is to close the window
     for event in pygame.event.get(): #event.get will get us all the events in our game.
         
-        if game_active:
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
 
+        if game_active:
             if event.type == pygame.KEYDOWN:#check if some key is pressed
                if event.key == pygame.K_SPACE and player_rect.bottom == 300:#check if the key pressed is equal to the space
                     player_gravity = -22 
@@ -53,10 +57,11 @@ while True:
         #we choose the surface and the coordinates(x,y) in where the surface will be displayed
         screen.blit(sky_surface, (0,0))
         screen.blit(ground_surface, (0,300))
-        pygame.draw.rect(screen, 'pink', score_rect)
-        pygame.draw.rect(screen, 'pink', score_rect, 10)
-        screen.blit(score_surf, score_rect)
-
+        #pygame.draw.rect(screen, 'pink', score_rect)
+        #pygame.draw.rect(screen, 'pink', score_rect, 10)
+        #screen.blit(score_surf, score_rect)
+        display_score()
+        
         #snail
         snail_rect.left -= 4
         if snail_rect.left < -70: snail_rect.left = 800#if the position of the snail is equal to -70 start again from 840 
