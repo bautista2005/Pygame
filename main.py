256b424c75a161156047573e21d18eb98ad80aee
bautista2005 <bachuv07@gmail.com>
2022-07-18 23:16:04 -0300

    my first commit

--- main.py
+++ main.py
@@ -7,47 +7,74 @@ pygame.init()#inizialice pygame, all the elements of pygame
 #width and height of the screen in tuple form
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption("Runner")#Changing the window name
-<<<<<<< HEAD
 
-clock = pygame.time.Clock()#creating an objet clock
-
-test_surface = pygame.Surface((100, 200))
-test_surface.fill("red")#"coloring" the test_surface
-=======
 clock = pygame.time.Clock()#creating an objet clockx
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)#(font type, font size)  
-snail_x_pos = 840
-snail_y_pos = 265
 
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
-text_surfaces = test_font.render('My game', False, 'black')
-snail_surface = pygame.image.load('graphics/snail/snail1.png')
->>>>>>> d13f600 (second commit)
+game_active = True
+
+sky_surface = pygame.image.load('graphics/Sky.png').convert_alpha()
+ground_surface = pygame.image.load('graphics/ground.png').convert_alpha()
+
+score_surf = test_font.render('My game', False, 'black')
+score_rect = score_surf.get_rect(center= (400, 50))
+
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surface.get_rect(midbottom= (800, 300))
+
+player_surface = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+player_rect = player_surface.get_rect(midbottom= (100, 300))#takes a surface and draw a rectangle around it
+
+player_gravity = 0
 
 #this is the game loop, we we will show our elements and update everything.
 while True:
     #for loop, we will be looking for all player inputs until the input is to close the window
-    for event in pygame.event.get(): #event.get will get us all the events.
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-    
-<<<<<<< HEAD
-    #attaching the test_surface to the display surface
-    #putting test_surface on the Display surface
-    #we choose the test_surface and the coordinates(x,y) in where the test surface will be displayed
-    screen.blit(test_surface, (0,0))
+    for event in pygame.event.get(): #event.get will get us all the events in our game.
+        
+        if game_active:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
 
-=======
-    screen.blit(sky_surface, (0,0))
-    screen.blit(ground_surface, (0,300))
-    screen.blit(text_surfaces, (325,50))
+            if event.type == pygame.KEYDOWN:#check if some key is pressed
+               if event.key == pygame.K_SPACE and player_rect.bottom == 300:#check if the key pressed is equal to the space
+                    player_gravity = -22 
 
-    snail_x_pos -= 4
-    if snail_x_pos < -70: snail_x_pos = 840#if the position of the snail is equal to -70 start again from 840 
-    screen.blit(snail_surface, (snail_x_pos, snail_y_pos))
->>>>>>> d13f600 (second commit)
+        else:
+            if event.type == pygame.KEYDOWN:
+                if  event.key == pygame.K_SPACE:
+                    game_active = True
+                    snail_rect.left = 800
+
+    if game_active:
+        #attaching _surface to the display surface
+        #putting surface on the Display surface
+        #we choose the surface and the coordinates(x,y) in where the surface will be displayed
+        screen.blit(sky_surface, (0,0))
+        screen.blit(ground_surface, (0,300))
+        pygame.draw.rect(screen, 'pink', score_rect)
+        pygame.draw.rect(screen, 'pink', score_rect, 10)
+        screen.blit(score_surf, score_rect)
+
+        #snail
+        snail_rect.left -= 4
+        if snail_rect.left < -70: snail_rect.left = 800#if the position of the snail is equal to -70 start again from 840 
+        screen.blit(snail_surface, snail_rect)
+
+        #player
+        player_gravity += 1
+        player_rect.top += player_gravity
+
+        if player_rect.bottom >= 300: player_rect.bottom = 300
+        screen.blit(player_surface, player_rect)
+
+        #collition
+        if snail_rect.colliderect(player_rect):
+            game_active = False        
+    else:
+        screen.fill('yellow')
     #updates the display surface, we are putting all our elements in the screen we made
     pygame.display.update()
     clock.tick(60)#we said to the program that the while loop cant run faster than 60 times per second
+             
